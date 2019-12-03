1aea6ccc1fd19563dba5628f55799d5a2db6b65b
rituraj.gupta <rituraj.gupta@vitrana.com>
2019-12-03 18:05:19 +0530

    new project for portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path:'', component:PortfolioComponent},
+  {path:'about', component:AboutComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
