f43320fec920e949d26388576bed7303823c9cf9
rituraj.gupta <rituraj.gupta@vitrana.com>
2019-12-04 12:47:58 +0530

    added resume, contact component. UI improvements.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path:'', component:PortfolioComponent},
   {path:'about', component:AboutComponent},
+  {path:'resume', component:ResumeComponent},
+  {path:'contact', component:ContactComponent}
 ];
 
 @NgModule({
@@ -17,11 +22,14 @@ const routes: Routes = [
     AppComponent,
     PortfolioComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
