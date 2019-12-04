eb23934271e5789f4cedca918ae41921494da45b
rituraj.gupta <rituraj.gupta@vitrana.com>
2019-12-04 11:29:54 +0530

    changes in about me component and css aligning

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   working = '<a href=\'www.vitrana.com\'>Vitrana</a>';
-  title = "Currently Working at "+ "<a href='www.vitrana.com'> + Vitrana + </a>"+ " as a Software engineer.";
+  title = "Currently Working at Vitrana as a Software engineer.";
 
   constructor() {
   }
